65ca7ce15bd50b6e3a6fe9088282d3f0d634c046
Cindy Xue <cindyxue1405@gmail.com>
2019-07-19 13:52:32 -0700

    fix

--- Report/extract.py
+++ Report/extract.py
@@ -9,8 +9,9 @@ with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
     for fileName in listOfFileNames:
         if fileName.endswith('.json'):
             zip_ref.extract(fileName, directory_to_extract_to)
-            md5 = os.path.splitext(fileName)[0]
-            oldName = directory_to_extract_to + '/' + fileName
+            # md5 = os.path.splitext(fileName)[0]
+            md5 = 'fe04c0fc835669bc27e75db4bebfa696'
+            oldName = directory_to_extract_to + fileName
             newName = directory_to_extract_to + '/' + md5 + '.json'
             print("old: " + oldName)
             print("new: " + newName)
