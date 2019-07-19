2735e0bc5b818a1ac1a8900aaac7c28a93aa281f
Cindy Xue <cindyxue1405@gmail.com>
2019-07-19 14:12:03 -0700

    debug

--- Report/extract.py
+++ Report/extract.py
@@ -1,10 +1,11 @@
 import os
 import zipfile
 
-path_to_zip_file = '/data/sandbox/Report_Dir/14/pe32/fe04c0fc835669bc27e75db4bebfa696.zip'
+path_to_zip_file = '/data/sandbox/Report_Dir/14/pe32/'
+name = 'fd8ebf23a27e9c6dfbf9ef7779eb2455.zip'
 directory_to_extract_to = '/home/cindy/SandboxML/Report/'
 
-with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+with zipfile.ZipFile(path_to_zip_file + zip_file_name, 'r') as zip_ref:
     listOfFileNames = zip_ref.namelist()
     for fileName in listOfFileNames:
         if fileName.endswith('.json'):
