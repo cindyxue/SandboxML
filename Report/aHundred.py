bd59b8fb69a7ca25bfb2ece0d8b1167b2d10dbf6
Cindy Xue <cindyxue1405@gmail.com>
2019-07-23 16:58:40 -0700

    extract 100

---
+++ Report/aHundred.py
@@ -0,0 +1,16 @@
+import os
+from shutil import copyfile
+
+folder = '/data/sandbox/cuckoo_reports/'
+targetFolder = '/home/cindy/cuckooml/sample_data/1_hundred_test/'
+if not os.path.exists(targetFolder):
+        os.makedirs(targetFolder)
+
+i = 0
+for item in os.listdir(folder):
+    if i > 100:
+        break
+    cur = folder + item
+    print(cur)
+    # copyfile(cur, targetFolder)
+    i += 1
