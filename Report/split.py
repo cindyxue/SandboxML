164c095d70e4b179e12d69f1fa22dbcb34ff4dfe
Cindy Xue <cindyxue1405@gmail.com>
2019-07-26 10:25:23 -0700

    add 500

--- Report/split.py
+++ Report/split.py
@@ -4,16 +4,18 @@ from shutil import copyfile
 folder = '/data/sandbox/sandbox_black_report/'
 targetFolder = '/home/cindy/cuckooml/sample_data/'
 f = 'black_1/'
-if not os.path.exists(targetFolder):
-        os.makedirs(targetFolder)
+if not os.path.exists(targetFolder + f):
+        os.makedirs(targetFolder + f)
 
 i = 0
 cnt = 1
 for item in os.listdir(folder):
     if i > 500:
         cnt += 1
-        f = 'black_' + str(cnt) + '/'
         i = 0
+        f = 'black_' + str(cnt) + '/'
+        if not os.path.exists(targetFolder + f):
+            os.makedirs(targetFolder + f)
     cur = folder + item
     print(targetFolder + f + item)
     copyfile(cur, targetFolder + f + item)
